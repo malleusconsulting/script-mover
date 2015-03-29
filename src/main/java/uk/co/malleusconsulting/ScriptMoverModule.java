d6d11b4a494240cd300c74df060c7b0a932e37bf
Chris Jennings <malleusconsulting@users.noreply.github.com>
2015-03-29 20:42:02 +0100

    Initial project setup.

---
+++ src/main/java/uk/co/malleusconsulting/ScriptMoverModule.java
@@ -0,0 +1,12 @@
+package uk.co.malleusconsulting;
+
+/**
+ * This class is optional and represents the configuration for the script-mover module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/script-mover</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class ScriptMoverModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
